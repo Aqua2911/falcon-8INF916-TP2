10bd8c21f0d232b666a51f1fb75dcb6e7004927f
CBerl139 <berlioz.clement.28@gmail.com>
2025-02-18 07:15:45 +0100

    added test case (not working yet)

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,6 +8,8 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "falcon.h"
+#include "Stream.h"
+
 #include "spdlog/spdlog.h"
 
 TEST_CASE("Can Listen", "[falcon]") {
@@ -78,4 +80,42 @@ TEST_CASE("Can Notify Disconnection", "[falcon]") {
     REQUIRE(!receiver->clients.empty());
     std::this_thread::sleep_for(std::chrono::seconds(3));
     REQUIRE(receiver->clients.empty());
+}
+
+TEST_CASE("Stream can send and receive data", "[stream]") {
+    auto sender = Falcon::Connect("127.0.0.1", 5556);
+    auto receiver = Falcon::Listen("127.0.0.1", 5555);
+
+    sender->ConnectTo("127.0.0.1", 5555);
+
+    std::string from_ip;
+    from_ip.resize(255);
+    std::array<char, 65535> buffer;
+    receiver->ReceiveFrom(from_ip, buffer);
+
+    // client-side creation of reliable stream
+    auto stream = receiver->CreateStream(1, true);
+
+
+
+    REQUIRE(stream != nullptr);
+
+    std::string message = "Test Stream Data";
+    std::span<const char> data(message.data(), message.size());
+
+    // Send data
+    stream->SendData(data);
+
+    // Simulate receiving the same data
+    stream->OnDataReceived(data);
+
+    //// Check if the pendingResends vector stores data correctly (only for reliable mode)
+    //if (stream->IsReliable()) {
+    //    REQUIRE_FALSE(stream->pendingResends.empty());
+    //    REQUIRE(std::equal(
+    //            stream->pendingResends.back().begin(),
+    //            stream->pendingResends.back().end(),
+    //            message.begin()
+    //    ));
+    //}
 }
